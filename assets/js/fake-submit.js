68e0c343c517d46514edc388d4c49d64e7b7e96e
Daniel Hanzlík <daniel.hanzlik@tul.cz>
2024-05-31 20:27:10 +0200

    faek submit hotov

--- assets/js/fake-submit.js
+++ assets/js/fake-submit.js
@@ -2,7 +2,13 @@ var form = document.getElementById('fake-submit');
 
 form.addEventListener('submit', function (event) { 
     event.preventDefault();
-    var username = document.getElementById('validationDefaultUsername').value;
-    console.log(username);
-    window.alert(username);
+    var jmeno = document.getElementById('jmeno').value;
+    var prijmeni = document.getElementById('prijmeni').value;
+    var email = document.getElementById('email').value;
+    var telefon = document.getElementById('telefon').value;
+    var kurz = document.getElementById('kurz').value;
+    var souhlas = document.getElementById('souhlas').checked;
+    
+    window.alert("Formulář byl úspěšně odeslán s nasledujícími údaji:\nJméno: " + jmeno + "\nPříjmení: " + prijmeni + "\nEmail: " + email + "\nTelefon: " + telefon + "\nKurz: " + kurz +
+        "\nTOS a GDPR: " + souhlas+"\nOčekávejte kontakt od našeho pracovníka na váš email nebo telefonní číslo.");
 })
